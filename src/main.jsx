c34012a9b6a183fa0749403757dc280fe8c6f11f
Sara Ferreira <sara.f.araujo12@gmail.com>
2024-03-22 12:22:31 -0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>HOME</div>
+  },
+  {
+    path: "/about",
+    element: <div>ABOUT</div>
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+    <App />
+  </React.StrictMode>,
+)
